e83dcd83b4c078a420a675aac872c6e6ec40cc41
Njagalavibe <patricolumu@gmail.com>
2023-07-04 22:16:02 +0300

    added drumKit.js file

---
+++ 01 - JavaScript Drum Kit/drumKit.js
@@ -0,0 +1,16 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    console.log(audio);
+    if (!audio) return; // stop the function from running
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    key.classList.add('playing');
+  };
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // skip all other properties that are not transform
+    this.classList.remove('playing')
+  }
+  let keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener("keydown", playSound);
